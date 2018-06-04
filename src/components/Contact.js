775f5be1c5d3780210310e1f3e5c8c12415a78f5
SmainKld <32145559+SmainKld@users.noreply.github.com>
2018-06-04 16:36:04 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Contact = () =>
   <div>
     <header className="App-header">
-
       <h1 className="App-title">Welcome to React</h1>
     </header>
     <p className="App-intro">

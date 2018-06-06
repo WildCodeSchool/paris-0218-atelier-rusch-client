5a2f77d04ea6e7e37d4773ceadee51546bdc2b52
samuel <samlambert221@gmail.com>
2018-06-06 17:09:52 +0200

    injection de data dans homepage, atelier et projets

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 const Contact = () =>
   <div>
-    <header className="App-header">
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
     <p className="App-intro">
-      Welcome to the contact!
+      Welcome to the contact Page!
     </p>
   </div>
 

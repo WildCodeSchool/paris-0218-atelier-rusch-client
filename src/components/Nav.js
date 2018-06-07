ec47ebb01063b6253598b5700fee63f31747d41b
Marie Malarme <malarmemarie@gmail.com>
2018-06-08 01:29:53 +0200

    styling the navbar container + use of flexbox

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,36 @@
 import React from 'react'
 
 const Nav = ({onPageChange}) =>
-  <div>
+  <div className="NavBar">
+    <div className="Logo">RUSCH</div>
+    <div className="NavBarLinks">
 
-    <button value='home'
-      onClick={onPageChange}>
-        Home
-    </button>
+      <button value='home'
+        onClick={onPageChange}>
+          Home
+      </button>
 
-    <button value='atelier'
-      onClick={onPageChange}>
-        Atelier
-    </button>
+      <button value='atelier'
+        onClick={onPageChange}>
+          Atelier
+      </button>
 
-    <button value='projets'
-      onClick={onPageChange}>
-        Projets
-    </button>
+      <button value='projets'
+        onClick={onPageChange}>
+          Projets
+      </button>
 
-    <button value='lab'
-      onClick={onPageChange}>
-        Lab Rusch
-    </button>
+      <button value='lab'
+        onClick={onPageChange}>
+          Lab Rusch
+      </button>
 
-    <button value='contact'
-      onClick={onPageChange}>
-        Contact
-    </button>
+      <button value='contact'
+        onClick={onPageChange}>
+          Contact
+      </button>
 
+    </div>
   </div>
 
 export default Nav

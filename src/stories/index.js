eee18431b02be2972eeff98a9c8091642d36df2c
Yoann Cribier <yoann_cribier@hotmail.com>
2018-06-11 17:46:08 +0200

    Article - stories added

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,8 +6,15 @@ import { linkTo } from '@storybook/addon-links'
 
 import { Button, Welcome } from '@storybook/react/demo'
 
+import Article from '../components/Article'
+import mockedArticles from '../mocks/articles.json'
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}><span role="img" aria-label= "joli emojis">😀 😎 👍 💯</span></Button>)
+
+storiesOf('Article', module)
+  .add('article 1', () => <Article article={mockedArticles[0]} />)
+  .add('article 2', () => <Article article={mockedArticles[1]} />)

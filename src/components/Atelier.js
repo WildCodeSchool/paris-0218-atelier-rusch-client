5ef8c74cb2373877bce770385e26c1140fb58f5e
Marie Malarme <malarmemarie@gmail.com>
2018-06-27 15:36:14 +0200

    setting the Atelier page + css enhancements

--- src/components/Atelier.js
+++ src/components/Atelier.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import './css/Atelier.css'
+import AtelierSection from './AtelierSection.js'
 
-const Atelier = (props) => {
+const Atelier = () => {
   return (
     <div>
-      <h2> Présentation de Atelier Rusch qui devra être administrable: </h2>
-      <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+
+      <AtelierSection title={'Notre vision'} bgColor="white">
+        <p>
+        Nous pensons que le design est une expertise qui permet de concevoir des projets responsables. Mais contrairement à des approches plus techniques, la nature transversal du design lui donne la capacité de faire dialoguer expertises variés, et de s’aventurer dans des domaines qui peuvent lui paraître éloigné, ou encore de compléter d’autres corps de métier. Évidemment, le design n’a pas réponse à tout, et se doit donc de d’avancer en coopération avec les savoir-faire et expertises propres au développement de chaque projets : qu’elles soient industrielles, sociales, culturelles, et bien sûr quotidienne.
+        </p>
+        <p className="quote">Nous sommes convaincus que la participation citoyenne est une des clefs de l’innovation. </p>
+        <p>Non pas par sa simple concertation, mais par son intégration dans la conception. Ainsi, les usagers deviennent moteur des transformations de leur environnements.
+        </p>
+      </AtelierSection>
+
+      <AtelierSection title={'Notre approche'} bgColor="white">
+        <p className="quote">Le design est une approche avant tout centrée sur les usagers.</p>
+        <p>
+        Nous les intégrons aux développement de nos projets en progressant à l’aide d’ateliers participatifs. Ils sont toujours uniques dans leur forme et leur fond, pour s’adapter à leur public, leurs enjeux, et leurs contextes. Ces ateliers nous accompagnent durant tout le développement du projet et condensent une approche de designer : depuis l’exploration d’idées, jusqu’à des chantiers participatifs, en passant par des phases de co-conception. En les utilisant comme des vecteurs de dialogue entre les acteurs et les usagers, nous produisons une matière qui mêle des qualités sensibles et techniques. Ainsi, nous nourrissons le développement de projets innovants, tout en restant au plus proches des besoins des usagers.
+        </p>
+      </AtelierSection>
+
+      <AtelierSection title={'Notre équipe'} bgColor="white">
+      </AtelierSection>
+
+      <AtelierSection title={'Notre histoire'} bgColor="white">
+      </AtelierSection>
+
+      <AtelierSection title={'Notre écosystème'} bgColor="#5aff6e">
+        <p>
+        L’atelier Rusch fonctionne en collaboration avec un éventail de savoir-faire, y compris techniques et industriels. Dans cette idée, nous avons installés notre bureau au sein d’ICIMontreuil, un atelier solidaire où se cotoient quotidiennement des artisans et makers de différents milieux.<br/>
+        Ainsi, nous avons accès à un parc de machine qui nous permettent de facilement prototyper nos projet pour expérimenter nos avancées échelle 1, de développer des outils de facilitations ou des scénographies, ou d’engager nos chantier participatif sur des points techniques difficilement abordables pour un grand public. De plus, nous tissons des partenariats avec les entreprises que nous y croisons quotidiennements, qu’elles soient artisanales ou industriels.
+        </p>
+      </AtelierSection>
+
     </div>
   )
 }

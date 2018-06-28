d6b0a6d42b74e97ff132c9dfcff7b4069371a7f8
Smain Kld <smainkld@gmail.com>
2018-06-28 10:34:30 +0200

    to be merged with pre-dev

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import './css/Carousel.css'
+import { Link } from '@reach/router'
 
 export const SlideShow = ({ articles }) => {
   articles.map(a => a.headerImage)
 
   const imagesElements = articles.map(article =>
-    <div key={article.id} onClick={() => console.log(`open modal for article ${article.id}`)}>
+    <div key={article.id}>
       <img src={article.headerImage} alt={article.title} />
       <p className="legend">{article.title}</p>
     </div>
   )
 
   return (
-    <Carousel autoPlay interval={5000} stopOnHover={true} infiniteLoop={true}>
-      {articles.length > 0 ? imagesElements : ''}
-    </Carousel>
-  )
+    <Link to={String(articles.id)}>
+      <Carousel autoPlay interval={5000} stopOnHover={true} infiniteLoop={true}>
+        {articles.length > 0 ? imagesElements : ''}
+      </Carousel>
+    </Link>
+    )
 }
+
+
+//onClick={() => console.log(`open modal for article ${article.id}`)}

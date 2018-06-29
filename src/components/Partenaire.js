b5420a2f860cbb20db10792eb56f848570b623eb
Marie Malarme <malarmemarie@gmail.com>
2018-06-29 23:42:45 +0200

    added partenaire component

---
+++ src/components/Partenaire.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Partenaire = () =>
+  <div className="PartenaireContainer">
+  </div>
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 
+import store from '../store.js'
+
 import Modale from '../components/Modale'
 import ArticleThumbnail from '../components/ArticleThumbnail'
 import mockedArticles from '../mocks/articles.json'
+import mockedMembers from '../mocks/members.json'
 import FormExemple from '../components/FormExemple'
 import ArticleForm from '../components/ArticleForm'
 import Atelier from '../components/Atelier'
+import EquipeMember from '../components/EquipeMember'
 
 import RedirectingBlockToAllArticles from '../components/RedirectingBlockToAllArticles'
 
@@ -18,6 +22,8 @@ import '../App.css'
 //   .add('normal', () => <NavBarLink />)
 //   .add('hover & active page', () => <NavBarLink />)
 
+const state = store.getState()
+
 storiesOf('Thumbnails', module)
   .add('article classic', () => <ArticleThumbnail className="ArticleThumbnailClassic" article={mockedArticles[0]} />)
   .add('article with star', () => <ArticleThumbnail className="ArticleThumbnailClassic ArticleThumbnailHasStar FilterBlack" article={mockedArticles[0]} />)
@@ -27,11 +33,11 @@ storiesOf('Modale', module)
   .add('Modale Article', () => <Modale article={mockedArticles[3]} />)
   .add('Modale Projet', () => <Modale article={mockedArticles[8]} />)
 
-storiesOf('FormExemple', module)
-  .add('exemple 1', () => <FormExemple />)
-
-storiesOf('ArticleForm', module)
-  .add('exemple 1', () => <ArticleForm />)
+// storiesOf('ArticleForm', module)
+//   .add('exemple 1', () => <ArticleForm />)
 
 storiesOf('Page Atelier', module)
   .add('Page Atelier', () => <Atelier />)
+
+storiesOf('Section Équipe', module)
+  .add('Team member', () => <EquipeMember member={mockedMembers[0]}/>)

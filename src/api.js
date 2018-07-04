178b698aa38e360da86a7a8cc400ca9cb1777c19
Marie Malarme <malarmemarie@gmail.com>
2018-07-04 16:28:01 +0200

    set up of the admin partenaires

--- src/api.js
+++ src/api.js
@@ -4,6 +4,24 @@ const getFilters = () => fetch('/filters').then(res => res.json())
 
 const getEquipe = () => fetch('/equipe').then(res => res.json())
 
+const getPartenaires = () => fetch('/partenaires').then(res => res.json())
+
+const newPartenaire = partenaire => fetch('/partenaires', {
+  method: 'post',
+  body: JSON.stringify(partenaire),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+const updatePartenaire = (id, partenaire) => fetch(`/partenaires/${id}`, {
+  method: 'put',
+  body: JSON.stringify(partenaire),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
 const newFilter = filter => fetch('/filters', {
   method: 'post',
   body: JSON.stringify(filter),
@@ -39,8 +57,11 @@ export default {
   getArticles,
   getFilters,
   getEquipe,
+  getPartenaires,
   newFilter,
   updateFilter,
+  newPartenaire,
+  updatePartenaire,
   newArticle,
   updateArticle
 }

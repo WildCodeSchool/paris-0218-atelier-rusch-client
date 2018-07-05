5f84fc04c9082d3e852b74f5da555e9790be83e2
Marie Malarme <malarmemarie@gmail.com>
2018-07-05 23:16:48 +0200

    adjustement of adminnavbar with new login feature

--- src/components/AdminNav.js
+++ src/components/AdminNav.js
@@ -28,7 +28,7 @@ class AdminNav extends Component {
       </Link>
     </div>
     <div className="NavBarLinksContainer">
-      <Link className={window.location.pathname === '/admin' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='' label='Articles'> Administration </Link>
+      <Link className={window.location.pathname === '/admin/dashboard' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='dashboard' label='Articles'> Administration </Link>
       <Link className={window.location.pathname === '/admin/articles' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='articles' label='Articles'> Articles </Link>
       <Link className={window.location.pathname === '/admin/filtres' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='filtres' label='Filtres'> Filtres</Link>
       <Link className={window.location.pathname === '/admin/equipe' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='equipe' label='Equipe'> Equipe</Link>

36f4a3f7453c9423233db237ef36f9d8bb121d76
Marie Malarme <malarmemarie@gmail.com>
2018-07-05 18:58:04 +0200

    adjusting the admin navbar

--- src/components/AdminNav.js
+++ src/components/AdminNav.js
@@ -13,11 +13,11 @@ const AdminNav = () =>
       </Link>
     </div>
     <div className="NavBarLinksContainer">
-      <Link className='NavBarLink' to='' label='Articles'> Administration </Link>
-      <Link className={window.location.pathname === '/admin/articles' ? 'NavBarLink active' : 'NavBarLink'} to='articles' label='Articles'> Articles </Link>
-      <Link className={window.location.pathname === '/admin/filtres' ? 'NavBarLink active' : 'NavBarLink'} to='filtres' label='Filtres'> Filtres</Link>
-      <Link className={window.location.pathname === '/admin/equipe' ? 'NavBarLink active' : 'NavBarLink'} to='equipe' label='Equipe'> Equipe</Link>
-      <Link className={window.location.pathname === '/admin/partenaires' ? 'NavBarLink active' : 'NavBarLink'} to='partenaires' label='Partenaires'> Partenaires </Link>
+      <Link className={window.location.pathname === '/admin' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='' label='Articles'> Administration </Link>
+      <Link className={window.location.pathname === '/admin/articles' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='articles' label='Articles'> Articles </Link>
+      <Link className={window.location.pathname === '/admin/filtres' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='filtres' label='Filtres'> Filtres</Link>
+      <Link className={window.location.pathname === '/admin/equipe' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='equipe' label='Equipe'> Equipe</Link>
+      <Link className={window.location.pathname === '/admin/partenaires' ? 'NavBarLinkAdmin active' : 'NavBarLinkAdmin'} to='partenaires' label='Partenaires'> Partenaires </Link>
     </div>
   </div>
 

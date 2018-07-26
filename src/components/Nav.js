56108af5292cee818642125887a94a6c65321bfe
Christopher Fremond <christopher.fremond@gmail.com>
2018-07-26 15:31:26 +0200

    reload after projets labrush switch

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,8 +16,14 @@ const Nav = ({onPageChange}) =>
 
     <div className="NavBarLinksContainer">
       <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/atelier' ? 'NavBarLink active' : 'NavBarLink'} to='/atelier'>Atelier</Link>
-      <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/projets' ? 'NavBarLink active' : 'NavBarLink'} to='/projets'>Projets</Link>
-      <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/lab' ? 'NavBarLink active' : 'NavBarLink'} to='/lab'>LabRusch</Link>
+      <Link onClick={() => {
+        window.scrollTo(0, 0)
+        window.location.reload()
+      }} className={window.location.pathname === '/projets' ? 'NavBarLink active' : 'NavBarLink'} to='/projets'>Projets</Link>
+      <Link onClick={() => {
+        window.scrollTo(0, 0)
+        window.location.reload()
+      }} className={window.location.pathname === '/lab' ? 'NavBarLink active' : 'NavBarLink'} to='/lab'>LabRusch</Link>
       <Link onClick={() => window.scrollTo(0, 0)} className={window.location.pathname === '/contact' ? 'NavBarLink active' : 'NavBarLink'} to='/contact'>Contact</Link>
     </div>
 
